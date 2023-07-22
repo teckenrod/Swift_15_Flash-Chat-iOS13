979bd0f19937d7aa578a4e11a17bbaaeb9690e14
teckenrod <treyeck2@gmail.com>
2023-07-22 13:46:50 -0500

    cleaned up keyboard animation

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -30,7 +30,9 @@ class ChatViewController: UIViewController {
     }
     
     func loadMessages() {
-        db.collection(K.FStore.collectionName).addSnapshotListener { querySnapshot, error in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
             self.messages = []
             if let e = error {
                 print(e)
@@ -54,7 +56,9 @@ class ChatViewController: UIViewController {
     
     @IBAction func sendPressed(_ sender: UIButton) {
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { error in
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender,
+                                                                      K.FStore.bodyField: messageBody,
+                                                                      K.FStore.dateField: Date().timeIntervalSince1970]) { error in
                 if let e = error {
                     print(e)
                 } else {
@@ -62,6 +66,7 @@ class ChatViewController: UIViewController {
                 }
             }
         }
+        messageTextfield.text = ""
     }
     
 
